506ceee33d063bd189fb20e22a63102d8f559855
NOEL-19 <noelrico1885@gmail.com>
2024-10-24 10:17:43 +0000

    Task Done

--- Tasksheet121.java
+++ Tasksheet121.java
@@ -1,1 +1,36 @@
+import static java.lang.Math.*;
 
+public class Tasksheet121 {
+
+    public static int add(int a, int b) {
+        return addExact(a,b);
+    }
+
+    public static int subtract(int a, int b) {
+        return subtractExact(a, b);
+    }
+
+    public static int multiply(int a, int b) {
+        return multiplyExact(a, b);
+    }
+
+    public static float divide(int a, int b) {
+        return floorDiv(a, b);
+    }
+
+    public static void main(String[] args) {
+        
+        int a = 50;
+        int b = 20;
+
+
+        System.out.println("Addition of "+ a + " and "+ b + " is "+ add(a, b));
+        System.out.println("Addition of "+ b + " and "+ a + " is "+ subtract(a, b));
+        System.out.println("Addition of "+ a + " and "+ b + " is "+ multiply(a, b)); 
+        System.out.println("Addition of "+ a + " and "+ b + " is "+ divide(a, b));
+
+
+
+
+    }
+}
